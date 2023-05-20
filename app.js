70bbe6bbd8673ee2afaf4853126275b3939ac929
Dinara Idrissova <Dinara.Idrissova@list.ru>
2023-05-19 23:36:03 -0400

    OS module and File System Module examples

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const path = require("path");
+const os = require("os");
+const fs = require("fs");
 const logger = require("./logger");
 
 // function sayHi() {
@@ -21,3 +23,20 @@ console.log(pathObj); //gives => {
 //   ext: '.js',
 //   name: 'app'
 // }
+
+//OS Module, object which has methods to work with a operating system
+const totalMemory = os.totalmem();
+const freeMemory = os.freemem();
+console.log(`Total Memory:${totalMemory}`);
+console.log(`Free Memory: ${freeMemory}`);
+
+//File System Module. object which has methods to work with a file system.All methods in this module comes in 2 forms: sync or blocking and async or non-blocking. In real-world applications ALWAYS work with async methods, non-block.
+//example with sync / blocking
+// const files = fs.readdirSync("./");
+// console.log(files); //[ '.git', 'app.js', 'logger.js' ]
+
+//example with async / non-block. We need to  define callback function to handle response, will get one of 2: error or data
+const files = fs.readdir("./", (err, files) => {
+  if (err) console.log("Error ", err);
+  else console.log("Files ", files); //will get array of file names
+});
