cb47d2929882a5c72207757bf64488af93a89fdc
Dinara Idrissova <Dinara.Idrissova@list.ru>
2023-05-22 17:35:42 -0400

    Extending EventEmitter

--- logger.js
+++ logger.js
@@ -1,8 +1,18 @@
-console.log(__dirname);
-console.log(__filename);
-function log(message) {
-  console.log(message + " module func");
+const EventEmitter = require("events");
+
+// console.log(__dirname);
+// console.log(__filename);
+//to give all the abilities(props and methods of EventEmitter class we use 'expents')
+class Logger extends EventEmitter {
+  log(message) {
+    //Send an HTTP request
+    console.log(message + " module func");
+
+    //Raise an event
+    this.emit("messageLocked", { id: 2, url: "http://" });
+  }
 }
 
 // module.exports.log = log;//exported as an object
-module.exports = log; //exported as a function
+// module.exports = log; //exported as a function
+module.exports = Logger; // export Logger class
